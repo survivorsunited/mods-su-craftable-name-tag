52ec8a08b858bbfcfd1d05df45aaa0fe87f89d09
wildone <max.barrass@gmail.com>
2025-06-22 04:06:42 +1000

    Redirect homepage to /docs/intro so wiki is the homepage

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,19 +1,10 @@
-import type {ReactNode} from 'react';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
+import {useEffect} from 'react';
+import {useHistory} from '@docusaurus/router';
 
-export default function Home(): ReactNode {
-  return (
-    <Layout title="SU Craftable Name Tag Wiki" description="Documentation for the SU Craftable Name Tag Minecraft mod">
-      <main style={{textAlign: 'center', marginTop: '4rem'}}>
-        <Heading as="h1">Welcome to the SU Craftable Name Tag Wiki</Heading>
-        <p style={{fontSize: '1.2rem'}}>Use the navigation bar to explore installation, usage, FAQ, and technical details.</p>
-        <p>
-          <a className="button button--primary button--lg" href="/docs/intro">
-            Go to Wiki
-          </a>
-        </p>
-      </main>
-    </Layout>
-  );
+export default function Home() {
+  const history = useHistory();
+  useEffect(() => {
+    history.replace('/docs/intro');
+  }, [history]);
+  return null;
 }
